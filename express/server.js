03ef1574abcb6d89e5754c47f50330efab4179af
riateam <elefanteblanco5@gmail.com>
2021-02-19 09:15:29 -0400

    Test

--- express/server.js
+++ express/server.js
@@ -27,7 +27,7 @@ router.post('/', (req, res) => {
   })
     .then(response => {
       res.status(200)
-      res.json(response.data)
+      res.json(req.body.id)
     })
     .catch(error => {
       console.log('Error with Axios profile res: ', error)
