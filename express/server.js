63dc339c823ce8c9dbaa53150b75aba64784bbc3
riateam <elefanteblanco5@gmail.com>
2021-02-19 09:40:35 -0400

    Test

--- express/server.js
+++ express/server.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
   const data = req.body;
-  const endpoint = `https://api.vimeo.com/videos/${data.id || 39619054}/`;
+  const endpoint = `https://api.vimeo.com/videos/${39619054}/`;
 
   axios({
     method: 'get',
@@ -23,7 +23,7 @@ router.post('/', (req, res) => {
   })
     .then(response => {
       res.status(200)
-      res.send(response.data)
+      res.send(req.body.id)
     })
     .catch(error => {
       console.log('Error with Axios profile res: ', error)
