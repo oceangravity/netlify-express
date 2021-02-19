ae910c79c72ca6d59aabdba671d345c77691d34f
riateam <elefanteblanco5@gmail.com>
2021-02-19 09:56:23 -0400

    Test

--- express/server.js
+++ express/server.js
@@ -16,7 +16,7 @@ router.post('/', (req, res) => {
   const endpoint = `https://api.vimeo.com/videos/${39619054}/`;
 */
   res.status(200)
-  res.json(req.body)
+  res.json(req.body.id)
 
 /*  axios({
     method: 'get',
